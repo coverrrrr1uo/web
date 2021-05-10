4f4899885204f3fef4ba53ac6ca7d6689bdef446
Fangjie Luo <fluo8@sheffield.ac.uk>
2021-05-10 22:51:49 +0000

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
 
+router.post('/imageRoute', function (req, res, next) {
+  // get random weather for a location
+  let imageData = req.body;
+  console.log(imageData);
+  //Check if the request data is empty
+  if (imageData == null) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(403).json({error: 403, reason: 'no image data provided'});
+  } else {
+    // send the data back
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(imageData));
+  }
+});
+
 
 
 module.exports = router;
