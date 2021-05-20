41f115ccb772ac487a47c51a943a714e2df24b82
Zizheng Jiang <zjiang32@sheffield.ac.uk>
2021-05-20 22:11:13 +0000

    fix bugs

--- public/javascripts/indexDB.js
+++ public/javascripts/indexDB.js
@@ -1,13 +1,13 @@
+let db1=null;
 
-let db;
 
 const Chatroom_DB_NAME= 'db_Chatroom';
 const Image_STORE_NAME = 'store_image';
 const History_STORE_NAME = 'store_history';
 
 async function initDatabase(){
-    if (!db) {
-        db = await idb.openDB(Chatroom_DB_NAME,2,{
+    if (!db1) {
+        db1 = await idb1.openDB(Chatroom_DB_NAME,2,{
             upgrade(upgradeDb, oldVersion, newVersion) {
                 if (!upgradeDb.objectStoreNames.contains(Image_STORE_NAME)) {
                     let imageDB = upgradeDb.createObjectStore(Image_STORE_NAME, {
@@ -30,40 +30,54 @@ async function initDatabase(){
 }
 window.initDatabase= initDatabase;
 
-async function storeCachedData(image, history) {
-    if (!db)
+async function storeCachedData(data) {
+    if (!db1)
         await initDatabase();
-    if (db) {
+    if (db1) {
         try{
-            let tx = await db.transaction(Image_STORE_NAME, 'readwrite');
+            let tx = await db1.transaction(Image_STORE_NAME, 'readwrite');
             let store = await tx.objectStore(Image_STORE_NAME);
-            await store.put(image);
+            await store.put(data);
             await tx.complete;
-
-            let tx1 = await db.transaction(History_STORE_NAME, 'readwrite');
-            let store1 = await tx.objectStore(History_STORE_NAME);
-            await store1.put(history);
-            await tx1.complete;
+            console.log('added item to the store! '+ JSON.stringify(data));
         } catch(error) {
             console.log('error: Could not store the element.' + error);
         };
     }
-    else localStorage.setItem('history', JSON.stringify(history));
+    else localStorage.setItem('image', JSON.stringify(data));
 }
 window.storeCachedData= storeCachedData;
 
-async function getCachedData(image,history) {
-    if (!db)
+async function storeHistoryData(data) {
+    if (!db1)
         await initDatabase();
-    if (db) {
+    if (db1) {
+        try{
+            let tx = await db1.transaction(History_STORE_NAME, 'readwrite');
+            let store = await tx.objectStore(History_STORE_NAME);
+            await store.put(data);
+            await tx.complete;
+            console.log('added item to the store! '+ JSON.stringify(data));
+        } catch(error) {
+            console.log('error: Could not store the element.' + error);
+        };
+    }
+    else localStorage.setItem('history', JSON.stringify(data));
+}
+window.storeHistoryData= storeHistoryData;
+
+async function getCachedData(image,history) {
+    if (!db1)
+        await initDatabase();
+    if (db1) {
         console.log('fetching: ' + image);
-        let tx = await db.transaction(Image_STORE_NAME, 'readonly');
+        let tx = await db1.transaction(Image_STORE_NAME, 'readonly');
         let store = await tx.objectStore(Image_STORE_NAME);
         let index = await store.index('image');
         let readingsList = await index.getAll(IDBKeyRange.only(image));
         await tx.complete;
 
-        let tx1 = await db.transaction(History_STORE_NAME, 'readonly');
+        let tx1 = await db1.transaction(History_STORE_NAME, 'readonly');
         let store1 = await tx1.objectStore(History_STORE_NAME);
         let index1 = await store1.index('history');
         let readingsList1 = await index1.getAll(IDBKeyRange.only(history));
